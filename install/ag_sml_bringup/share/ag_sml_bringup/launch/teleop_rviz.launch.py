f5bc24aa289705fb55fec398851e922d022ac83f
Bruce Digney <bdigney@shaw.ca>
2024-02-24 12:33:14 -0700

    fixed lidar mount

---
+++ install/ag_sml_bringup/share/ag_sml_bringup/launch/teleop_rviz.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os.path
+
+def generate_launch_description():
+
+    # rviz_config_file = PathJoinSubstitution(
+    #     [FindPackageShare("ag_sml_bringup"), "rviz2", "ag_with_scan.rviz"]
+    # )
+
+    teleop_node = Node(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        parameters=[],
+        remappings=[
+             ('/cmd_vel', '/diffbot_base_controller/cmd_vel_unstamped'),
+            ],
+        prefix = 'xterm -e'
+    )
+    rviz2_node = Node(
+            package='rviz2',
+            namespace='',
+            executable='rviz2',
+            name='rviz2',
+            # arguments=['-d', rviz_config_file]
+            arguments=['-d', "/home/bdigney/ros2_roboclaw/src/ag_sml_bringup/rviz2/ag_with_scan.rviz"]
+        )
+    nodes = [teleop_node, 
+         rviz2_node,      
+    ]
+    return LaunchDescription(nodes)
